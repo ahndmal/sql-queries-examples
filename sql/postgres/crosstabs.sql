bd9ef3aa47f89671320f430fb9daa95b5a4fce90
Andrii Maliuta <quadr988@gmail.com>
2024-08-15 16:59:57 +0000

    cases

--- sql/postgres/crosstabs.sql
+++ sql/postgres/crosstabs.sql
@@ -17,3 +17,33 @@ ORDER BY office',
         chocolate bigint,
         strawberry bigint,
         vanilla bigint);
+
+---
+
+SELECT *
+FROM crosstab('SELECT
+                 station_name,
+                 date_part(''month'', observation_date),
+                 percentile_cont(.5)
+                    WITHIN GROUP (ORDER BY max_temp)
+               FROM temperature_readings
+               GROUP BY station_name,
+               date_part(''month'', observation_date)
+               ORDER BY station_name',
+         'SELECT month
+          FROM xgenerate_series(1,12) month')
+AS (station varchar(50),
+    jan numeric(3,0),
+    feb numeric(3,0),
+    mar numeric(3,0),
+    apr numeric(3,0),
+    may numeric(3,0),
+    jun numeric(3,0),
+    jul numeric(3,0),
+    aug numeric(3,0),
+    sep numeric(3,0),
+    oct numeric(3,0),
+    nov numeric(3,0),
+    dec numeric(3,0)
+);
+
