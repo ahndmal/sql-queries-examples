91359b08cb3118939949d047ecfd9d4fc8fb694e
Andrii Maliuta <quadr988@gmail.com>
2024-08-15 16:53:45 +0000

    crosstabs

---
+++ sql/postgres/crosstabs.sql
@@ -0,0 +1,19 @@
+
+CREATE EXTENSION tablefunc;
+
+SELECT *
+ FROM crosstab('SELECT 
+    office,
+   flavor,
+    xcount(*)
+FROM ice_cream_survey
+GROUP BY office, flavor
+ORDER BY office',
+ 'SELECT flavor
+    FROM ice_cream_survey
+    GROUP BY flavor
+    ORDER BY flavor')
+    AS (office varchar(20),
+        chocolate bigint,
+        strawberry bigint,
+        vanilla bigint);
